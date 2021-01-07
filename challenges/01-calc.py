3e4cb2383c4b12456a83ecd3cf48a68ffadfbe42
alexjbustillos <alexjbustillos@gmail.com>
2021-01-07 15:53:43 -0500

    [missing last two]could not figure it out

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,34 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+# define functions
+def add(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def mult(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    return num1 / num2
+# take user input
+print("Select an operator")
+print("1.Add")
+print("2.Sub")
+print("3.Mult")
+print("4.Div")
+
+choice = input("add/sub/mult/div: ")
+
+num1 = int(input("First number:"))
+num2 = int(input("Second number:"))
+
+if choice == 'add':
+    print(add(num1, num2))
+elif choice == 'sub':
+    print(sub(num1, num2))
+elif choice == 'mult':
+    print(mult(num1, num2))
+elif choice == 'div':
+    print(div(num1, num2))
+else:
+    print("Invalid input")
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,10 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+def bank(balance):
+    question = input('Would you like to check your balance?\n')
+    if question == yes:
+        return balance
+    elif question == no:
+        return input('What would you like to do?')
+    elif 
